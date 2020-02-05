11c61bb9af383133f7ed7786006e2c1a52a0fa5c
kfolwick <kayla.folwick@gmail.com>
2020-02-05 10:54:19 -0800

    adds controller methods to posts_controller.rb and tests with rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-        devise_for :users
+  resources :posts
+  devise_for :admins, path: 'admins'
+  devise_for :users, path: 'users'
        get '*path', to: 'pages#index', constraints: ->(request){ request.format.html? }
        root to: 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_03_213654) do
+ActiveRecord::Schema.define(version: 2020_02_04_222831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "age"
+    t.string "postal_code"
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "event_name"
+    t.string "address"
+    t.string "category"
+    t.text "event_description"
+    t.string "start_time"
+    t.string "end_time"
+    t.string "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

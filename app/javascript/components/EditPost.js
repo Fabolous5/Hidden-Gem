7cafa54701ad4c0cd0d680f84b6e679d70125f79
Morganne Kayler <mkayler242@gmail.com>
2020-02-12 14:40:14 -0800

    adds photo upload component

--- app/javascript/components/EditPost.js
+++ app/javascript/components/EditPost.js
@@ -1,14 +1,17 @@
 import React from "react"
 import MainApp from './MainApp'
+import ActiveStorageProvider from 'react-activestorage-provider'
 
 class EditPost extends React.Component {
     constructor(props){
         super(props)
-        console.log(props);
         this.state = {
-
+          post:{}
         }
         this.getPost()
+    }
+    handleSubmit = (post)=>{
+      this.setState({ post: post})
     }
     getPost = () =>{
         const { id } = this.props.match.params
@@ -19,22 +22,65 @@ class EditPost extends React.Component {
             }
         })
         .then((post)=>{
-            this.setState({post})
+            this.setState({post:post})
         })
     }
 
-    render(){
-        return(
-            <React.Fragment>
-                {this.state.post &&
-                    <h1>{this.state.post.event_name}</h1>
-                }
-                {!this.state.post &&
-                    <h2>loading</h2>
-                }
-            </React.Fragment>
-        )
-    }
-}
+    render () {
+     const{ post } = this.state
+     return (
+       <React.Fragment>
+       <h1>Post: {post.event_name} </h1>
+         { post && post.photo_url &&
+           <div>
+             <h2>The photo is: </h2>
+             <img src={post.photo_url} />
+           </div>
+         }
+         <ActiveStorageProvider
+           endpoint={{
+             path: `/posts/${post.id}`,
+             model: 'Post',
+             attribute: 'photo',
+             method: 'PUT',
+           }}
+           onSubmit={this.handleSubmit}
+           render={({ handleUpload, uploads, ready }) => (
+             <div>
+               <input
+                 type="file"
+                 disabled={!ready}
+                 onChange={e => handleUpload(e.currentTarget.files)}
+               />
 
-export default EditPost
+               {uploads.map(upload => {
+                 switch (upload.state) {
+                   case 'waiting':
+                     return <p key={upload.id}>Waiting to upload {upload.file.name}</p>
+                   case 'uploading':
+                     return (
+                       <p key={upload.id}>
+                         Uploading {upload.file.name}: {upload.progress}%
+                       </p>
+                     )
+                   case 'error':
+                     return (
+                       <p key={upload.id}>
+                         Error uploading {upload.file.name}: {upload.error}
+                       </p>
+                     )
+                   case 'finished':
+                     return (
+                       <p key={upload.id}>Finished uploading {upload.file.name}</p>
+                     )
+                 }
+               })}
+             </div>
+           )}
+         />
+       </React.Fragment>
+     );
+   }
+ }
+
+ export default EditPost

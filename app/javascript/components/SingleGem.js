ee369aefc9375d888ad8edff067f897233bd6eb7
jlopez0108 <jclopez0108@gmail.com>
2020-02-11 16:20:42 -0800

    adds marker and popup to map

--- app/javascript/components/SingleGem.js
+++ app/javascript/components/SingleGem.js
@@ -2,16 +2,8 @@ import React from "react"
 import {Map, TileLayer, Marker, Popup} from 'react-leaflet'
 import L from "leaflet"
 import 'leaflet/dist/leaflet.css';
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
-let DefaultIcon = L.icon({
-            iconUrl: icon,
-            shadowUrl: iconShadow
-        });
-        L.Marker.prototype.options.icon = DefaultIcon;
 
-
-
+L.Icon.Default.imagePath = "https://unpkg.com/leaflet@1.5.0/dist/images/"
 
 
 class SingleGem extends React.Component{
@@ -24,8 +16,6 @@ class SingleGem extends React.Component{
     }
 
     render() {
-
-
         const accessToken = 'pk.eyJ1IjoiamxvcGV6MDEwOCIsImEiOiJjazZjam9oN20wYzRwM21tbGdtcjVqM2hnIn0.Xrqyk3Y6h9sqaEssl3qwHw'
 
         const tileUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}'
@@ -33,13 +23,12 @@ class SingleGem extends React.Component{
         const attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
 
         const maxZoom = 19
-
+        L.Icon.Default
 
     return (
 
     <div>
         <div>
-
 
             <h1>Event Name: </h1>
                 <h2>Date: </h2>
@@ -63,6 +52,11 @@ class SingleGem extends React.Component{
           id='mapbox/streets-v11'
           accessToken={accessToken}
         />
+
+        <Marker position={this.state.mapCenter}>
+         <Popup>You HiddenGem is here!!!</Popup>
+       </Marker>
+
       </Map>
       <h1>Description of the event: </h1>
         </div>

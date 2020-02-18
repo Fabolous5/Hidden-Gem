ded0fad2bbe9cab3a5cb11afd828750e54ebc1b8
maryredlinger <mary@redlinger.net>
2020-02-18 15:24:08 -0800

    adds to last commit

--- app/javascript/components/NewPost.js
+++ app/javascript/components/NewPost.js
@@ -89,7 +89,7 @@ const NewPost = (props) =>{
                 </fieldset>
 */}
                 <Link to="/">
-                    <button onClick={props.handleSubmit} type="submit" className="btn btn-primary">Submit Your Gem!</button>
+                    <button onClick={props.handleSubmit} type="submit" className="btn btn-warning">Submit Your Gem!</button>
                 </Link>
 
             </Form>

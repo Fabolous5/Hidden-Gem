942564137b9d8270f759261e5f15adebe99a5a4c
maryredlinger <mary@redlinger.net>
2020-02-18 13:11:47 -0800

    adds user profile gems, and styling throughout app

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import SingleGem from './SingleGem'
 
 import { NavItem, NavLink, Nav } from 'reactstrap'
 import {Link} from 'react-router-dom';
@@ -6,7 +7,7 @@ import {Link} from 'react-router-dom';
 const Home = (props) => {
 
     const taskSize = {"maxHeight": "500px", "overflowY": "scroll"}
-    const image = {"background-image": "url('https://upload.wikimedia.org/wikipedia/commons/1/15/Sandiego_skyline_at_night.JPG')", "image": "no-repeat"}
+    const image = {"background-image": "url('https://upload.wikimedia.org/wikipedia/commons/1/15/Sandiego_skyline_at_night.JPG')", "image": "no-repeat", "height": "600px"}
     return(
         <div>
             {props.user.signed_in &&
@@ -14,25 +15,38 @@ const Home = (props) => {
                 <div  className="jumbotron" style={image}>
                     <h1 className="display-3 text text-white">Welcome to Hidden Gems, {props.user.first_name}!</h1>
                     <p className="lead text-white">Discover low-key events in the San Diego area.</p>
-                    <hr className="my-4 "/>
-                    <p className= "text-red" >For locals. By locals.</p>
+
+                    <p className= "text-white" >For locals. By locals.</p>
                 </div>
 
                 <div  className="btn-toolbar d-flex justify-content-around">
                 {props.posts && props.posts.map((post, index)=>{
                     return (
-                        <div style={taskSize} key={index} role="toolbar">
-                            <div style={{width: "300px", height: "200px"}} className="card border-primary mb-3" >
+                        <div   role="toolbar">
+                            <div style={{width: "600px", height: "700px"}} className="card border-primary mb-3" >
                             { post.photo_url &&
                             <img src={post.photo_url} className="card-img-top"/>
                         }
-                                <div className="card-header"></div>
+                                <div className="card-header">
+                                    <h1>{post.event_name}</h1>
+                                </div>
+                                <div key={index}>
+                                <div style={taskSize}>
                                 <div className="card-body">
-                                    <h4 className="card-title">{post.event_name}</h4>
                                     <p className="card-text">{post.event_description}</p>
-                                        <Link to={`/Show/${post.id}`}>Click to see more details</Link>
-                                        <Link to={`/EditPost/${post.id}`}> Edit Gem</Link>
+                                    <h6 className="card-title">{post.address}</h6>
+                                    <h6 className="card-title">Start Time: {post.start_time}</h6>
+                                    <h6 className="card-title">End Time: {post.end_time}</h6>
+                                    <h6 className="card-title">Date: {post.date}</h6>
 
+                                    {/*<Link to={`/EditPost/${post.id}`}> Edit Gem</Link>
+                                        <Link to={`/Show/${post.id}`}>Click to see more details</Link>*/}
+                                        <hr />
+                                        <br />
+                                        <SingleGem />
+
+                                </div>
+                                </div>
                                 </div>
                             </div>
                         </div>
@@ -47,9 +61,9 @@ const Home = (props) => {
                     <h1 className="display-3 text text-white">Welcome to Hidden Gems!</h1>
                     <p className="lead text-white">Discover low-key events in the San Diego area.</p>
                     <hr className="my-4 "/>
-                    <p className= "text-red" >For locals. By locals.</p>
+                    <p className= "text-white" >For locals. By locals.</p>
                     <p className="lead">
-                    <a className="btn btn-warning btn-lg" href="/users/sign_up" role="button">Learn more</a>
+                    <a className="btn btn-warning btn-lg" href="/users/sign_up" role="button">Sign up now!</a>
                     </p>
                 </div>
 
@@ -58,20 +72,24 @@ const Home = (props) => {
             <div  className="btn-toolbar d-flex justify-content-around">
             {props.posts && props.posts.map((post, index)=>{
                 return (
-                    <div style={taskSize} key={index} role="toolbar">
-                        <div style={{width: "300px", height: "200px"}} className="card border-primary mb-3" >
+                    <div  key={index} role="toolbar">
+                        <div style={{width: "700px", height: "600px"}} className="card border-primary mb-3" >
 
-                            <div className="card-header"></div>
+                            <div className="card-header">
+                                <h1>{post.event_name}</h1>
+                            </div>
+
                             <div className="card-body">
-                                <h4 className="card-title">{post.event_name}</h4>
-                                <p className="card-text">{post.event_description}</p>
-                                    <Link to={`/posts/${post.id}`}>Click to see more details</Link>
+
+                                <a href="/users/sign_in">Sign in to see more</a>
                                     <br/>
-                                    <Link to={`/EditPost/${post.id}`}> Edit Gem</Link>
-                                    <br/>
+                                    <hr />
+                                    <br />
+                                    <SingleGem />
+
 
                             </div>
-                        </div>
+                            </div>
                     </div>
                 )
             })}
--- app/javascript/components/UserProfile.js
+++ app/javascript/components/UserProfile.js
@@ -1,31 +1,76 @@
 import React from "react"
+import {Link} from 'react-router-dom'
+import SingleGem from './SingleGem'
 
 const UserProfile = (props) => {
+    const taskSize = {"maxHeight": "500px", "overflowY": "scroll"}
+    // console.log("id here", props.posts);
+    // console.log(props.user.current_user_id);
+    //
+    const {userPost} = props.posts.map
 
-
+    // const {userPost} = props.posts.filter(post => {
+    //     post.user_id === 2})
+    // console.log(userPost);
     return(
         <div>
             <h1>Hello, {props.user.first_name}!</h1>
-            
-                <div className="btn-toolbar d-flex justify-content-around" role="toolbar">
-                    <div style={{width: "300px"}} className="d-flex justify-content-around card text-white bg-primary mb-3" >
-                        <img style={{width: "200px"}} src="https://p0.pikrepo.com/preview/699/108/silhouette-of-person-in-white-background.jpg" alt="Card image" />
-                        <div className="card-header">name: {props.user.first_name} {props.user.last_name}</div>
-                            <div className="card-body">
-                <h4 className="card-title">postal code: {props.user.postal_code}</h4>
-                <p className="card-text">email: {props.user.email}</p>
+            <h3>Look at all of your beautiful Gems!</h3>
+            <hr />
+
+            <div className="btn-toolbar d-flex justify-content-around" role="toolbar">
+                {/*<div style={{width: "300px"}} className="d-flex justify-content-around card text-white bg-primary mb-3" >
+                    <img style={{width: "200px"}} src="https://p0.pikrepo.com/preview/699/108/silhouette-of-person-in-white-background.jpg" alt="Card image" />
+                    <div className="card-header">name: {props.user.first_name} {props.user.last_name}</div>
+                    <div className="card-body">
+                        <h4 className="card-title">postal code: {props.user.postal_code}</h4>
+                        <p className="card-text">email: {props.user.email}</p>
+                    </div>
+                </div>*/}
+            {/*<div style={{width: "300px"}} className="d-flex justify-content-around card text-white bg-primary mb-3">
+                <div className="card-header">
+                     <h1 className="card-title">Edit Profile</h1>
+                </div>
+                <div className="card-body">
+                    <a href="/edituserprofile" className="btn btn-secondary>click here">click here</a>
+                </div>
+            </div> */}
+            </div>
+        {/* filter over post to find id === id*/}
+        <div  className="btn-toolbar d-flex justify-content-around">
+        {props.posts.map((post, index) =>{
+
+            return (
+                <div>
+                {post.user_id == props.user.current_user_id &&
+                <div  key={index} role="toolbar">
+                    <div style={{width: "600px", height: "700px"}} className="card border-primary mb-3" >
+                    { post.photo_url &&
+                        <img src={post.photo_url} className="card-img-top"/>
+                    }
+                        <div className="card-header"><h1>{post.event_name}</h1></div>
+                        <div style={taskSize}>
+                        <div className="card-body">
+                            <p className="card-text">{post.event_description}</p>
+                            <h6 className="card-title">{post.address}</h6>
+                            <h6 className="card-title">Start Time: {post.start_time}</h6>
+                            <h6 className="card-title">End Time: {post.end_time}</h6>
+                            <h6 className="card-title">Date: {post.date}</h6>
+                            <Link to={`/EditPost/${post.id}`}> Edit Gem</Link>
+                                {/*<Link to={`/Show/${post.id}`}>Click to see more details</Link>*/}
+                                <hr />
+                                <br />
+                                <SingleGem />
+
+                        </div>
+                    </div>
                     </div>
                 </div>
-                {/*<div style={{width: "300px"}} className="d-flex justify-content-around card text-white bg-primary mb-3" >
-                      <div className="card-header">
-                      <h1 className="card-title" >
-                      Edit Profile</h1>
-                      </div>
-                      <div className="card-body">
-                        <a href="/edituserprofile" className="btn btn-secondary>click here">click here</a>
-                 </div>
-                 </div>*/}
-
+            }
+            </div>
+                )
+            })
+        }
         </div>
 
 
